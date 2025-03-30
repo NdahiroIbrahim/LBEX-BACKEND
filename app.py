1ee67658c4d067314f392f1434eb6454a00199c8
hassan <hassanibrahimndahiro@gmail.com>
2025-03-30 13:47:23 +0300

    initialized db migrations

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, request, jsonify, send_file
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,7 +10,9 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 print(f"Database Path: {app.config['SQLALCHEMY_DATABASE_URI']}")
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 CORS(app, supports_credentials=True)
+
 class Waitlist(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
