323b1ffe1296d3a745a5a7f93b796898059f6b4e
hassan <hassanibrahimndahiro@gmail.com>
2025-04-30 14:22:17 +0300

    Changed db to postgre

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = ""
-    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(BASE_DIR, 'instance', 'app.db')}"
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
